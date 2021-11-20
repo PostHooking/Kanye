605e6e4fc397ccb353d76ea8bd925392d388bb5d
1x2x3 <91387271+1x2x3@users.noreply.github.com>
2021-11-20 13:23:56 -0500

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -2,6 +2,7 @@ place = game.PlaceId
 games = {
     HoopsLife = {7540728600,7540728417,7540727946,7797523964};
     HoopsDemo = {360589910};
+    MyPark = {5614075443,4851363266,5626636612};
 }
 
 for i,v in pairs(games) do
